aab72b9eb19bcc4995ed7045c960e06357a5f46d
sehoi <sehoi.kim@bugs.co.kr>
2017-12-05 11:02:50 +0900

    Update model to Keras API 2

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
-from keras.layers import Input, merge
-from keras.layers.convolutional import Convolution1D
+from keras.layers import Input, Dense, Dropout, Flatten, concatenate, dot
+from keras.layers.convolutional import Conv1D
 from keras.layers.embeddings import Embedding
 from keras.layers.pooling import MaxPooling1D, GlobalMaxPooling1D
-from keras.layers.core import Dense, Dropout, Merge, Flatten
 from keras.models import Sequential, Model
-from gensim.models import Word2Vec
-from keras import initializations
+from keras import initializers
 from keras.engine.topology import Layer
+
+import gensim
 import keras.backend as K
 import numpy as np
 import pdb
@@ -15,7 +15,7 @@ class SimLayer(Layer):
 
 	def __init__(self, output_dim, **kwargs):
 		self.output_dim = output_dim
-		self.init = initializations.get('glorot_uniform')
+		self.init = initializers.get('glorot_uniform')
 		super(SimLayer, self).__init__(**kwargs)
 
 	def build(self, input_shape):
@@ -32,7 +32,7 @@ class SimLayer(Layer):
 
 
 def load_embeddings(path, vocab):
-	word2vec = Word2Vec.load_word2vec_format(path, binary=True)
+	word2vec = gensim.models.KeyedVectors.load_word2vec_format(path, binary=True)
 	dim = word2vec['common'].shape[0]
 	embedding = np.zeros((len(vocab), dim))
 	rand_vec = np.random.uniform(-0.25, 0.25, dim)
@@ -52,18 +52,18 @@ def load_embeddings(path, vocab):
 def Model1(dim, max_ques_len, max_ans_len, vocab_lim, embedding):
 	inp_q = Input(shape=(max_ques_len,))
 	embedding_q  = Embedding(vocab_lim, dim, input_length=max_ques_len, weights=[embedding], trainable=False)(inp_q)
-	conv_q= Convolution1D(100, 5, border_mode='same', activation='relu')(embedding_q)
+	conv_q= Conv1D(100, 5, padding='same', activation='relu')(embedding_q)
 	conv_q = Dropout(0.25)(conv_q)
 	pool_q = GlobalMaxPooling1D()(conv_q)
 
 	inp_a = Input(shape=(max_ans_len,))
 	embedding_a  = Embedding(vocab_lim, dim, input_length=max_ans_len, weights=[embedding], trainable=False)(inp_a)
-	conv_a = Convolution1D(100, 5, border_mode='same', activation='relu')(embedding_a)
+	conv_a = Conv1D(100, 5, padding='same', activation='relu')(embedding_a)
 	conv_a = Dropout(0.25)(conv_a)
 	pool_a = GlobalMaxPooling1D()(conv_a)
 
 	#sim = SimLayer(1)([pool_q, pool_a])
-	sim = merge([Dense(100, bias=False)(pool_q), pool_a], mode='dot')
+	sim = dot([Dense(100, use_bias=False)(pool_q), pool_a], (1, 1))
 	# print pool_a, pool_q
 
 	# model1 = merge([pool_q, pool_a, sim], mode='concat')
@@ -73,7 +73,7 @@ def Model1(dim, max_ques_len, max_ans_len, vocab_lim, embedding):
 	# return model
 	
 
-	model_sim = merge([pool_q, pool_a, sim], mode='concat')
+	model_sim = concatenate([pool_q, pool_a, sim])
 	print model_sim
 
 	# #model_final = Flatten()(model_sim)
@@ -82,7 +82,7 @@ def Model1(dim, max_ques_len, max_ans_len, vocab_lim, embedding):
 	model_final = Dropout(0.5)(model_final)
 	model_final = Dense(1, activation='sigmoid')(model_final)
 
-	model = Model(input=[inp_q, inp_a], output=[model_final])
+	model = Model(inputs=[inp_q, inp_a], outputs=[model_final])
 	print(model.output_shape)
 	model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
 	print model.summary()
